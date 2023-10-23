de0199e6f1d6080fc0ab233df94d62d261596cc3
Honey1315 <honeykhatri10@gmail.com.com>
2023-10-24 00:48:40 +0530

    registerPage

--- src/pages/register.js
+++ src/pages/register.js
@@ -1,53 +1,66 @@
 import React, { useEffect, useState } from "react";
-import styles from "@/styles/login.module.css";
+import styles from "@/styles/register.module.css";
 import { useRouter } from "next/router";
 import Link from "next/link";
+import { treasureBox } from "@/assets/TreasureBox";
+import axios from "axios"
+import {toast, ToastContainer} from "react-toastify";
+import 'react-toastify/dist/ReactToastify.css';
 
 export default function Register() {
-    const [creds, setDetails] = useState({ id: "", name: "", pass: "", pass2: "", phone: "" });
+    const [creds, setDetails] = useState({ email: "", fullname: "", password: "", username: "", phone: "" });
     const [isValidSize, setValidity] = useState(true);
     const [error, setError] = useState(false);
     const router = useRouter();
 
-    function register() {
-        console.log(creds);
-        if (creds.id.length == 0) setError("Please Enter Your Celesta ID");
-        else if (creds.name.length == 0) setError("Please Enter Your Name");
-        else if (creds.phone.length != 10) setError("Please Enter Valid Indian Phone Number");
-        else if (creds.pass.length < 5 || creds.pass.length > 32)
-            setError("Password must be between 5 and 32 characters");
-        else if (creds.pass != creds.pass2) setError("Passwords Don't Match");
-        else {
-            setError(false);
-            console.log("Okay");
-            crypto.subtle
-                .digest("SHA-256", new TextEncoder().encode(creds.pass))
-                .then((res) => Array.from(new Uint8Array(res)))
-                .then((res) => {
-                    fetch(`http://localhost:8080/register`, {
-                        method: "POST",
-                        headers: {
-                            "Content-Type": "application/json;charset=utf-8",
-                        },
-                        body: JSON.stringify({
-                            ...creds,
-                            pass: undefined,
-                            pass2: undefined,
-                            hash: res.map((b) => b.toString(16).padStart(2, "0")).join(""),
-                        }),
-                    })
-                        .then((res) => {
-                            if (res.status == 409) {
-                                alert("User Already Exists");
-                                document.getElementById("redir").classList.add(styles.redText);
-                            }
-                            console.log(res);
-                            if (res.status == 200) router.push('/dashboard');
-                        })
-                        .catch((error) => alert(error));
-                });
+    const register = async () => {
+        // console.log("OK");
+        // if (creds.id.length == 0) setError("Please Enter Your Celesta ID");
+        // else if (creds.fullname.length == 0) setError("Please Enter Your Name");
+        // else if (creds.phone.length != 10) setError("Please Enter Valid Indian Phone Number");
+        // else if (creds.pass.length < 5 || creds.pass.length > 32)
+        //     setError("Password must be between 5 and 32 characters");
+        // else {
+        //     setError(false);
+        //     console.log("Okay");
+        //     crypto.subtle
+        //         .digest("SHA-256", new TextEncoder().encode(creds.pass))
+        //         .then((res) => Array.from(new Uint8Array(res)))
+        //         .then((res) => {
+        //             fetch(`http://localhost:8080/register`, {
+        //                 method: "POST",
+        //                 headers: {
+        //                     "Content-Type": "application/json;charset=utf-8",
+        //                 },
+        //                 body: JSON.stringify({
+        //                     ...creds,
+        //                     pass: undefined,
+        //                     hash: res.map((b) => b.toString(16).padStart(2, "0")).join(""),
+        //                 }),
+        //             })
+        //                 .then((res) => {
+        //                     if (res.status == 409) {
+        //                         alert("User Already Exists");
+        //                         document.getElementById("redir").classList.add(styles.redText);
+        //                     }
+        //                     console.log(res);
+        //                     if (res.status == 200) router.push('/dashboard');
+        //                 })
+        //                 .catch((error) => alert(error));
+        //         });
+        // }
+        // console.log(error);
+        try {
+            const response = await axios.post("https://4291-212-8-243-131.ngrok-free.app/api/auth/register", creds)
+            if (response.data.success === false)
+                toast.error(response.data.message)
+            else {
+                router.push("/login")
+            }
+
+        } catch (error) {
+            toast.error("Something went wrong!");
         }
-        console.log(error);
     }
 
     useEffect(() => {
@@ -56,133 +69,93 @@ export default function Register() {
 
     return (
         <>
-            <div className="aurora-background blurred" />
+            <ToastContainer autoClose={2000}/>
             <div className={styles.mainContainer}>
-                {!isValidSize ? (
-                    <div className={styles.loginBox}>
-                        Please use a large enough screen (Minimum: 250px x 512px)
+                <div
+                    className={styles.registerBox}
+                    onKeyUp={(event) => {
+                        if (event.key == "Enter") register();
+                    }}
+                >
+                    <div className={styles.title}>
+                        Register
                     </div>
-                ) : (
-                    <div
-                        className={styles.registerBox}
-                        onKeyUp={(event) => {
-                            if (event.key == "Enter") authenticate();
-                        }}
-                    >
-                        <div className={styles.title}>Register for The Event of Your Life</div>
-
-                        <div className={styles.disclaimer}>
-                            Disclaimer: Incorrect details may Disqualify you from the Prize
-                        </div>
-
-                        <div className={styles.inputDiv}>
-                            Celesta ID
-                            <input
-                                type="username"
-                                onChange={(s) => {
-                                    if (s.target.value.match(/[^0-9a-zA-Z]/g) != null)
-                                        s.target.value = s.target.value.substring(
-                                            0,
-                                            s.target.value.length - 1
-                                        );
-                                    setDetails({ ...creds, id: s.target.value });
-                                }}
-                            />
-                        </div>
-
-                        <div className={styles.inputDiv}>
-                            Full Name
-                            <input
-                                type="name"
-                                onChange={(s) => {
-                                    setDetails({ ...creds, name: s.target.value });
-                                }}
-                            />
-                        </div>
-
-                        <div className={styles.inputDiv}>
-                            Phone Number
-                            <input
-                                type="text"
-                                onChange={(s) => {
-                                    if (
-                                        s.target.value.length > 10 ||
-                                        s.target.value.match(/[^0-9]/g) != null
-                                    )
-                                        s.target.value = s.target.value.substring(
-                                            0,
-                                            s.target.value.length - 1
-                                        );
-                                    setDetails({ ...creds, phone: s.target.value });
-                                }}
-                            />
-                        </div>
-
-                        <div className={styles.inputDiv}>
-                            Password
-                            <input
-                                type="password"
-                                onChange={(s) => {
-                                    if (
-                                        s.target.value.match(
-                                            /[^0-9a-zA-Z~`!@#$%^&*()_+={}|:;"'<,>.?\[\]\\\/-]/g
-                                        ) != null
-                                    )
-                                        s.target.value = s.target.value.substring(
-                                            0,
-                                            s.target.value.length - 1
-                                        );
-                                    if (
-                                        s.target.value.length > 0 &&
-                                        creds.pass2.length > 0 &&
-                                        s.target.value != creds.pass2
-                                    )
-                                        setError("Passwords Don't Match");
-                                    else setError(false);
-                                    setDetails({ ...creds, pass: s.target.value });
-                                }}
-                            />
-                        </div>
-
-                        <div className={styles.inputDiv}>
-                            Repeat Password
-                            <input
-                                type="password"
-                                onChange={(s) => {
-                                    if (
-                                        s.target.value.match(
-                                            /[^0-9a-zA-Z~`!@#$%^&*()_+={}|:;"'<,>.?\[\]\\\/-]/g
-                                        ) != null
-                                    )
-                                        s.target.value = s.target.value.substring(
-                                            0,
-                                            s.target.value.length - 1
-                                        );
-                                    if (
-                                        s.target.value.length > 0 &&
-                                        creds.pass.length > 0 &&
-                                        s.target.value != creds.pass
-                                    )
-                                        setError("Passwords Don't Match");
-                                    else setError(false);
-                                    setDetails({ ...creds, pass2: s.target.value });
-                                }}
-                            />
-                        </div>
-
-                        <div className={`${styles.redText} ${error ? "" : styles.invisible}`}>
-                            {error ? error : "Noice"}
-                        </div>
-
-                        <div className={styles.submit} onClick={() => register()}>
-                            Register
-                        </div>
-
-                        <Link className={styles.registerRedir} id="redir" href="/login">
-                            Already Registered 🥳, Click to Login
-                        </Link>
+                    <div className={styles.inputDiv}>
+                        <input
+                            className={styles.inputField}
+                            type="name"
+                            placeholder="Full Name"
+                            onChange={(s) => {
+                                setDetails({ ...creds, fullname: s.target.value });
+                            }}
+                        />
                     </div>
-                )}
+                    <div className={styles.inputDiv}>
+                        <input
+                            type="email"
+                            className={styles.inputField}
+                            placeholder="Email"
+                            onChange={(s) => {
+                                setDetails({ ...creds, email: s.target.value });
+                            }}
+                        />
+                    </div>
+                    <div className={styles.inputDiv}>
+                        <input
+                            type="text"
+                            className={styles.inputField}
+                            placeholder="Username"
+                            onChange={(s) => {
+                                setDetails({ ...creds, username: s.target.value });
+                            }}
+                        />
+                    </div>
+                    <div className={styles.inputDiv}>
+                        <input
+                            type="password"
+                            className={styles.inputField}
+                            placeholder="Create New Password"
+                            onChange={(s) => {
+                                setDetails({ ...creds, pass: s.target.value });
+                            }}
+                        />
+                    </div>
+                    <div className={styles.inputDiv}>
+                        <input
+                            type="text"
+                            className={styles.inputField}
+                            placeholder="Mobile Number"
+                            onChange={(s) => {
+                                if (
+                                    s.target.value.length > 10 ||
+                                    s.target.value.match(/[^0-9]/g) != null
+                                )
+                                    s.target.value = s.target.value.substring(
+                                        0,
+                                        s.target.value.length - 1
+                                    );
+                                setDetails({ ...creds, phone: s.target.value });
+                            }}
+                        />
+                    </div>
+
+                    <div className={`${styles.redText} ${error ? "" : styles.invisible}`}>
+                        {error ? error : "Noice"}
+                    </div>
+
+                    <div>
+                        <button onClick={() => register()}
+                            className={styles.submit}>Register</button>
+                    </div>
+
+                    <Link className={styles.registerRedir} id="redir" href="/login">
+                        Already Registered?
+                    </Link>
+
+                </div>
+                <div className={styles.imageContainer}>
+                    <div dangerouslySetInnerHTML={{ __html: treasureBox }}></div>
+                </div>
             </div>
         </>
     );
