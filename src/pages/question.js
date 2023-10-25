80e7606be836fc14261c52ce7a4cdb7251585294
Honey1315 <honeykhatri10@gmail.com.com>
2023-10-25 21:17:35 +0530

    questionPage Fixes

--- src/pages/question.js
+++ src/pages/question.js
@@ -2,31 +2,53 @@ import React from 'react'
 import styles from "@/styles/question.module.css";
 import Image from 'next/image';
 import { Navbar } from '@/componenets';
+import Box from '@mui/material/Box';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
+import Modal from '@mui/material/Modal';
 const question = () => {
     let i = 1, q = 1, score = 40;
+    const [open, setOpen] = React.useState(false);
+    const handleOpen = () => setOpen(true);
+    const handleClose = () => setOpen(false);
     return (
         <>
         <Navbar/>
+        <div>
+        <Modal
+            open={open}
+            onClose={handleClose}
+        >
+        <Box className={styles.modal}>
+          <Typography  fontWeight="700" fontSize="25px">
+            Hint
+          </Typography>
+          <Typography >
+            Duis mollis, est non commodo luctus, nisi erat porttitor ligula.
+          </Typography>
+        </Box>
+      </Modal>
+    </div>
         <div className={styles.mainContainer}>
             <div className={styles.scoreBox}>
                 <Image src="/assets/Vector.svg"
                     width={23}
                     height={25}
                 ></Image>
+                <div className={styles.qNo}>
+                        L0{i} Q0{q}
+                    </div>
                 <div>
                     Your Score: {score}
                 </div>
             </div>
             <div className={styles.questionDiv}>
                 <div className={styles.descrpBox}>
-                    <div className={styles.qNo}>
-                        L0{i} Q0{q}
-                    </div>
-                    <p>There was a time when this wouldn't have bothered her. The fact that it did actually bother her bothered her even more. What had changed in her life that such a small thing could annoy her so much for the entire day? She knew it was ridiculous that she even took notice of it, yet she was still obsessing over it as she tried to fall asleep.
-                    There was a time when this wouldn't have bothered her. The fact that it did actually bother her bothered her even more. What had changed in her life that such a small thing could annoy her so much for the entire day? She knew it was ridiculous that she even took notice of it, yet she was still obsessing over it as she tried to fall asleep.
-                    There was a time when this wouldn't have bothered her. The fact that it did actually bother her bothered her even more. What had changed in her life that such a small thing could annoy her so much for the entire day? She knew it was ridiculous that she even took notice of it, yet she was still obsessing over it as she tried to fall asleep.
-                    There was a time when this wouldn't have bothered her. The fact that it did actually bother her bothered her even more. What had changed in her life that such a small thing could annoy her so much for the entire day? She knew it was ridiculous that she even took notice of it, yet she was still obsessing over it as she tried to fall asleep.
-                    There was a time when this wouldn't have bothered her. The fact that it did actually bother her bothered her even more. What had changed in her life that such a small thing could annoy her so much for the entire day? She knew it was ridiculous that she even took notice of it, yet she was still obsessing over it as she tried to fall asleep.
+                    <p>There was a time when this wouldnt have bothered her. The fact that it did actually bother her bothered her even more. What had changed in her life that such a small thing could annoy her so much for the entire day? She knew it was ridiculous that she even took notice of it, yet she was still obsessing over it as she tried to fall asleep.
+                    There was a time when this wouldnt have bothered her. The fact that it did actually bother her bothered her even more. What had changed in her life that such a small thing could annoy her so much for the entire day? She knew it was ridiculous that she even took notice of it, yet she was still obsessing over it as she tried to fall asleep.
+                    There was a time when this wouldnt have bothered her. The fact that it did actually bother her bothered her even more. What had changed in her life that such a small thing could annoy her so much for the entire day? She knew it was ridiculous that she even took notice of it, yet she was still obsessing over it as she tried to fall asleep.
+                    There was a time when this wouldnt have bothered her. The fact that it did actually bother her bothered her even more. What had changed in her life that such a small thing could annoy her so much for the entire day? She knew it was ridiculous that she even took notice of it, yet she was still obsessing over it as she tried to fall asleep.
+                    There was a time when this wouldnt have bothered her. The fact that it did actually bother her bothered her even more. What had changed in her life that such a small thing could annoy her so much for the entire day? She knew it was ridiculous that she even took notice of it, yet she was still obsessing over it as she tried to fall asleep.
                     </p>
                 </div>
                 <div className={styles.questionImage}>
@@ -44,7 +66,7 @@ const question = () => {
                 >
                 </input>
                 <div>
-                <button className={styles.hint}>
+                <button className={styles.hint} onClick={handleOpen}>
                         <div className={styles.CheckContent}>
                             <Image src="/assets/Bulb.svg" width={18} height={18} />
                             <div>Hint</div>
